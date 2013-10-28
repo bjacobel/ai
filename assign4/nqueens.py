7e7322f6620ffc0fd0171c2fb5c8b701eaadd3ba
Brian Jacobel <bjacobel@gmail.com>
2013-10-27 22:09:15 -0400

    Did diagonal checking a colossally stupid way. Think I see a better one. Committing so I can roll back if neccesary.

--- assign4/nqueens.py
+++ assign4/nqueens.py
@@ -22,22 +22,38 @@ class Queen:
             usedRows.append(queen.row)
             usedColumns.append(queen.column)
 
-        # Check horizontal and vertical: there sould be exactly one instance of this row and column in queens    
+        # Check horizontal and vertical: there sould be exactly one instance of this row and column in queens
+        # This is easy to do, so we'll do it before the diagonal checking    
         if usedRows.count(self.row) == 1 and usedColumns.count(self.column) == 1:
+            
             # check diagonals (hard)
-            diagRow = self.row
-            diagColumn = self.column
-            while(diagRow <= self.boardSize-1 and diagColumn <= self.boardSize-1):
-                diagRow++
-                diagColumn++
+            downDiagRow = upDiagRow = self.row
+            downDiagColumn = upDiagColumn = self.column
+
+            # trace each diagonal the row is in back the the left side of the board
+            # trace the downward-slanting diagonal up as far as it will go and the
+            #   upward-slanting diagonal down as far as it will go
+            while downDiagRow > 0 and downDiagColumn > 0:
+                downDiagRow -= 1
+                downDiagColumn -= 1
+            while upDiagRow < self.boardSize - 1 and upDiagColumn > 0:
+                upDiagRow += 1
+                upDiagColumn -= 1
+
+            # follow upDiag northeast until it runs out, check if there's people there at each step
+            while upDiagRow > 0 and upDiagColumn < self.boardSize - 1:
+                upDiagRow -= 1
+                upDiagColumn += 1
                 for queen in queens:
-                    if(queen.row == diagRow and queen.column == diagColumn)
+                    if queen.row == upDiagRow and queen.column == upDiagColumn:
                         return False
-            while(diagRow >= 0 and diagColumn >= 0):
-                diagRow--
-                diagColumn--
+
+            # follow downDiag southeast until it runs out, check if there's people there at each step
+            while downDiagRow < self.boardSize - 1 and downDiagColumn < self.boardSize - 1:
+                downDiagRow += 1
+                downDiagColumn += 1
                 for queen in queens:
-                    if(queen.row == diagRow and queen.column == diagColumn)
+                    if queen.row == downDiagRow and queen.column == downDiagColumn:
                         return False
 
             return True
@@ -54,7 +70,7 @@ def drawboard(numQueens):
 
 
     if numQueens < 10:
-        qLabel += " Q"
+        qLabel = " Q"
     else:
         qLabel = "Q"
     hLineLen =  (7 * (numQueens)) + 1
@@ -62,13 +78,13 @@ def drawboard(numQueens):
     for i in xrange(0, hLineLen):
         hLine += "-"
 
-    print hLine
+    print(hLine)
     for i in xrange(0, numQueens*3):
         for j in xrange(0, numQueens):
             print("|     "),
         print("|")
         if i % 3 == 2:
-            print hLine
+            print(hLine)
 
 
 def min_conflicts(queens, maxSteps):
@@ -96,11 +112,8 @@ def main():
     for i in xrange(0, numQueens):
         queens.append(Queen(numQueens))
 
-    # draw the solved board
-    drawboard(numQueens)
-
     for queen in queens:
-        print queen
+        print(queen)
         if queen.isValid(queens):
             print("(That's a valid position!)")
 
